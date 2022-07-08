b045208ce1881aed8363fa3662cf12db5fe31143
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-07-08 10:57:24 +0100

    added bolded props to the open button

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,12 @@ import logo from "../images/logo1.png";
 const Header = () => {
   return (
     <header className='p-3 shadow-md px-8'>
-    <div className='flex justify-between items-center max-w-3xl mx-auto'>
-      <img className='w-20' src={logo} alt='' />
-      <button className='bg-[#7805A7] text-white rounded-md text-sm md:text-base p-2 px-4 font-thin tracking-wider'>
-        Open App
-      </button>
-    </div>
+      <div className='flex justify-between items-center max-w-3xl mx-auto'>
+        <img className='w-20' src={logo} alt='' />
+        <button className='bg-[#7805A7] text-white rounded-md text-sm md:text-base p-2 px-4 font-normal tracking-wider'>
+          Open App
+        </button>
+      </div>
     </header>
   );
 };
