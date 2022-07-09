e919163b5d75915237a3fa79804295549e0a3e94
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-07-09 15:59:13 +0100

    new

--- src/components/main/vendor.js
+++ src/components/main/vendor.js
@@ -16,13 +16,14 @@ const Vendor = () => {
             Vendors
           </h1>
         </div>
-        <div className='mt-12 text-gray-700'>
+        <div className='mt-12 text-gray-700 shadow-lg'>
           <div className='bg-purple-200 flex  space-x-6 md:grid md:grid-cols-5  text-xs p-4 rounded-md font-semibold w-full whitespace-nowrap overflowX'>
             <h4>Name</h4>
             <h4 className=''>Email</h4>
             <h4 className=''>Mobile Number</h4>
             <h4 className=''>Business Name</h4>
             <h4>Status</h4>
+            <h4>Action</h4>
           </div>
           <div>
             {vendors.map((items, i) => {
@@ -65,75 +66,8 @@ const Vendor = () => {
                 </div>
               );
             })}
-            ;
           </div>
         </div>
-        {/* <div className={`${showNote ? "category" : "category hider"} overflow`}>
-          <div className='bg-white shadow-md rounded-md p-4 text-gray-700'>
-            <img
-              className='bg-[#7805A7] p-2 ml-auto rounded-md'
-              src={close}
-              alt=''
-              onClick={() => setshowNote(false)}
-            />
-            <h1 className='text-center text-xl sm:text-2xl font-semibold my-3'>
-              New Notification
-            </h1>
-            <form action='' className='sm:w-5/6 mx-auto'>
-              <label htmlFor='category' className='text-sm'>
-                Subject
-              </label>
-              <input
-                type='text'
-                className='block bg-gray-100 p-2 rounded-md my-2 w-full font-sm mb-4'
-                placeholder='Subject of your notification'
-                value={sub}
-                onChange={(e) => setSub(e.target.value)}
-              />
-              <label htmlFor='category' className='text-sm'>
-                Message Details
-              </label>
-              <textarea
-                name=''
-                id=''
-                cols='30'
-                rows='10'
-                value={message}
-                placeholder='Notification Details'
-                className='block bg-gray-100 p-2 rounded-md my-2 w-full font-sm mb-4'
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <label htmlFor='category' className='text-sm'>
-                Select Receiver
-              </label>
-              <select
-                name=''
-                id=''
-                className='block bg-gray-100 p-2 rounded-md my-2 w-full font-sm capitalize mb-4'
-                value={receiver}
-                onChange={(e) => setReceiver(e.target.value)}
-              >
-                <option value='all'>all</option>
-                <option value='vendors'>vendors</option>
-                <option value='customers'>customers</option>
-              </select>
-
-              <button
-                className='bg-[#7805A7] text-white rounded-md text-sm md:text-base p-2 px-4 font-normal tracking-wider w-full my-2'
-                onClick={addNotification}
-              >
-                Broadcast Notification
-              </button>
-              <button
-                className='border-[#7805A7] border-2 text-[#7805A7] rounded-md text-sm md:text-base p-2 px-4 font-normal tracking-wider w-full my-2'
-                onClick={addNotification}
-              >
-                Save as Draft
-              </button>
-            </form>
-          </div>
-        </div> */}
-        {/* <NoteDetails {...noteObj} /> */}
       </main>
     </>
   );
